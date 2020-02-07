511113a212bb60568a34da3b4c283d462fd9c204
Adam Smith <38594717+asmitheyexe@users.noreply.github.com>
2020-02-07 11:01:38 -0600

    getting the backend set up

---
+++ backend/src/db.js
@@ -0,0 +1,11 @@
+// FILE HANDLES DB CONNECTION
+const {Prisma} = require('prisma-binding');
+const db = new Prisma({
+    typeDefs: 'src/generated/prisma.graphql',
+    endpoint: process.env.PRISMA_ENDPOINT,
+    secret: process.env.PRISMA_SECRET,
+    debug: false
+
+});
+
+module.exports = db;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,17 @@
-// let's go!
+// Start node server
+require('dotenv').config({path: 'variables.env'});
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+
+//TODO use express middleware to handle Cookies (JWT)
+// TODO use expres middleware to populate current User
+
+server.start({
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    }
+}, deets => console.log(`Server is running on port http://localhost:${deets.port}`));
